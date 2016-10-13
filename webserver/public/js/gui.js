c58907c685be6117d160c04b96168bf73414a2bd
SgtTed <gitkraken@hoelweb.com>
2016-10-13 22:48:15 +0200

    Completed marker definitions in marker.js. Added marker-fids.js. minterface.js is now gui.js

---
+++ webserver/public/js/gui.js
@@ -0,0 +1,190 @@
+/* global MissionIntelApp */
+// THE MINTERFACE!
+MissionIntelApp.GUI = function () {
+
+    var WIDTH = window.innerWidth - 1;
+    var HEIGHT = window.innerHeight - 1;
+    var mapCanvas;
+    //var canvas;
+    var menuDiv;
+    var mapContext;
+    var context;
+    var srcObjectNames = {AWACS: "AWACS", JSTAR: "JSTAR", HUMINT: "HUMINT", GEOINT: "GEOINT", SIGINT: "SIGINT"};
+    var forceObjectNames = {INFANTRY: "Infantry", ARMOR: "Armor", AIRASSETS: "Air-Assets"};            // MOVE THIS ARRAY TO DCSUNIT LATER?
+    var mapObj = new Image();
+    var mapZ = (Object.keys(srcObjectNames).length + 2 * Object.keys(forceObjectNames).length) * -1;
+
+    /**
+     * Initialize GUI
+     */
+    this.initialize = function () {
+
+        ///////////////////////////////////////////
+        // Set up all CANVASes and DIVs
+        menuDiv = document.createElement("div");
+        menuDiv.width = WIDTH;
+        menuDiv.height = HEIGHT;
+        menuDiv.id = "div-menu";
+        menuDiv.style = "z-index: 1; position:absolute; left:0px; top:0px;";
+        document.body.appendChild(menuDiv);
+
+        mapCanvas = document.createElement("canvas");
+        mapCanvas.id = "canvas-map";
+        mapCanvas.style = "z-index:" + mapZ + "; position:absolute; left:0px; top:0px;";
+        mapCanvas.width = WIDTH;
+        mapCanvas.height = HEIGHT;
+        document.body.appendChild(mapCanvas);
+        mapContext = mapCanvas.getContext("2d");
+
+        ///////////////////////////////////////////
+        // Assign GUI menu to DIV
+        var menu = new dat.GUI({autoPlace: false});
+        var customContainer = document.getElementById('div-menu');
+        customContainer.appendChild(menu.domElement);
+
+        var f1 = menu.addFolder('SOURCES');
+        var srcObjects = {};
+        for (var i in srcObjectNames) {
+
+            // Generate a canvas for each source NOTE!!! THERE IS SOMETHING STRANGE GOING ON! SHOULD NOT USE mapCanvas??
+            mapCanvas = document.createElement("canvas");
+            mapCanvas.id = "source-canvas-" + i;
+            mapCanvas.style = "z-index: " + (-i) + "; position:absolute; left:0px; top:0px;";
+            mapCanvas.width = WIDTH;
+            mapCanvas.height = HEIGHT;
+            document.body.appendChild(mapCanvas);
+            mapContext = mapCanvas.getContext("2d");
+
+            // Add an object and definition
+            srcObjects[i] = true;
+
+            // Add button and define function
+            var btn = f1.add(srcObjects, i);
+            btn.onChange(function () {
+
+                if (srcObjects[i])
+                    document.getElementById("source-canvas-" + i).style.visibility = "visible";
+                else
+                    document.getElementById("source-canvas-" + i).style.visibility = "hidden";
+
+            });
+        }
+
+        var f2 = menu.addFolder('BLUEFOR');
+        var bforObjects = {};
+        for (var i in forceObjectNames) {
+
+            // Generate a canvas for each source
+            mapCanvas = document.createElement("canvas");
+            mapCanvas.id = "bluefor-canvas-" + i;
+            mapCanvas.style = "z-index: " + (-i) + "; position:absolute; left:0px; top:0px;";
+            mapCanvas.width = WIDTH;
+            mapCanvas.height = HEIGHT;
+            document.body.appendChild(mapCanvas);
+            mapContext = mapCanvas.getContext("2d");
+
+            // Add an object and definition
+            bforObjects[i] = true;
+
+            // Add button and define function
+            var btn = f2.add(bforObjects, i);
+            btn.onChange(function () {
+
+                if (bforObjects[i])
+                    document.getElementById("bluefor-canvas-" + i).style.visibility = "visible";
+                else
+                    document.getElementById("bluefor-canvas-" + i).style.visibility = "hidden";
+
+            });
+        }
+
+        var f3 = menu.addFolder('REDFOR');
+        var rforObjects = {};
+        for (var i in forceObjectNames) {
+
+            // Generate a canvas for each source
+            mapCanvas = document.createElement("canvas");
+            mapCanvas.id = "redfor-canvas-" + i;
+            mapCanvas.style = "z-index: " + (-i) + "; position:absolute; left:0px; top:0px;";
+            mapCanvas.width = WIDTH;
+            mapCanvas.height = HEIGHT;
+            document.body.appendChild(mapCanvas);
+            mapContext = mapCanvas.getContext("2d");
+
+            // Add an object and definition
+            rforObjects[i] = true;
+
+            // Add button and define function
+            var btn = f3.add(rforObjects, i);
+            btn.onChange(function () {
+
+                if (rforObjects[i])
+                    document.getElementById("redfor-canvas-" + i).style.visibility = "visible";
+                else
+                    document.getElementById("redfor-canvas-" + i).style.visibility = "hidden";
+
+            });
+        }
+
+        // Set all folder to open by default
+        f1.open();
+        f2.open();
+        f3.open();
+
+        console.log("--> GUI initialized");
+
+        // Draw Map
+        mapObj.src = 'resources/img/map.jpg';
+        mapContext.drawImage(mapObj, -4000, -3000);
+
+        console.log("--> MAP Drawn");
+        ///////////////////////////////////////////
+
+    };
+
+    /**
+     * Add unit
+     * @param {DCSUnit} markerHash
+     */
+    this.addMarker = function (markerHash) {
+
+        MS.setStandard("APP6");
+
+        testHash = "SFG-UCI----D";
+        var canvasElement = new MS.symbol(markerHash, {size: 90}).getMarker().asCanvas();
+
+
+    };
+
+    /**
+     * Remove all units from view
+     */
+    this.clearUnits = function () {
+        context.clearRect();
+    };
+
+    this.initialize();
+
+    //this.clearUnits();
+    //this.addUnit(testUnit);
+
+    var newMarker = new MissionIntelApp.Marker(
+            // Regular Fixed-wing Marker: SFAPMFA-----
+            MissionIntelApp.Marker.CodingScheme.Warfighting,
+            MissionIntelApp.Marker.Affiliation.Friend,
+            MissionIntelApp.Marker.BattleDim.Air,
+            MissionIntelApp.Marker.Status.Present,
+            MissionIntelApp.Marker.FunctionID.MilitaryFixedWingAttack_strike,
+            MissionIntelApp.Marker.Modifier1.None,
+            MissionIntelApp.Marker.Modifier2.None,
+            MissionIntelApp.Marker.x = 5000,
+            MissionIntelApp.Marker.y = 5000
+    );
+
+    console.log("newMarker = "+newMarker.markerHash());
+    
+    //MS.setStandard("APP6");
+    //var markerCanvasElement = new MS.symbol(newMarker.markerHash(), {size: 90}).getMarker().asCanvas();
+    
+    
+};
---
+++ webserver/public/js/marker.js
@@ -0,0 +1,88 @@
+MissionIntelApp.Marker = function (codingScheme, affiliation, battleDimension, status, functionID, modifier1, modifier2, x, y) {
+    this.codingScheme = codingScheme;   // Allways 'S'
+    this.affiliation = affiliation;
+    this.battleDim = battleDimension;
+    this.status = status;
+    this.functionID = functionID;
+    this.modifier1 = modifier1;         // HQ, Task Force etc..
+    this.modifier2 = modifier2;         // Squad, Platoon etc..
+    this.x = x;
+    this.y = y;
+
+    this.markerHash = function () {
+        return "" + this.codingScheme + this.affiliation + this.battleDim + this.status + this.functionID + this.modifier1 + this.modifier2;
+    };
+};
+
+MissionIntelApp.Marker.CodingScheme = {
+    Warfighting: "S"
+};
+
+MissionIntelApp.Marker.Affiliation = {
+    Pending: "P",
+    Unknown: "U",
+    AssumedFriendly: "A",
+    Friend: "F",
+    Neutral: "N",
+    Suspect: "S",
+    Hostile: "H",
+    Joker: "J",
+    Faker: "K",
+    None: "O"
+};
+
+MissionIntelApp.Marker.BattleDim = {
+    Space: "P",
+    Air: "A",
+    Ground: "G",
+    SeaSurface: "S",
+    SeaSubSurface: "U",
+    SOF: "F"
+};
+
+MissionIntelApp.Marker.Status = {
+    None: "-",
+    Present: "P",
+    Planned: "A",
+    Assumed: "A"
+};
+
+MissionIntelApp.Marker.Modifier1 = {
+    None: "-",
+    HQ: "A",
+    TaskForceHQ: "B",
+    FeintDummyHQ: "C",
+    FeintDummyTaskForceHQ: "D",
+    TaskForce: "E",
+    FeintDummy: "F",
+    FeintDummyTaskForce: "G",
+    Installation: "H",
+    Mobility: "M",
+    TowedArray: "N"
+};
+
+MissionIntelApp.Marker.Modifier2 = {
+    None: "-",
+    Team: "A",
+    Crew: "A",
+    Squad: "B",
+    Section: "C",
+    Platoon: "D",
+    Detachment: "D",
+    Company: "E",
+    Battery: "E",
+    Troop: "E",
+    Battalion: "F",
+    Squadron: "F",
+    Regiment: "G",
+    Group: "G",
+    Brigade: "H",
+    Division: "I",
+    CorpsO: "J",
+    MEF: "J",
+    Army: "K",
+    ArmyGroup: "L",
+    Front: "L",
+    Region: "M",
+    Command: "N"
+};
