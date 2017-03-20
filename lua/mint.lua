aa47b8730a27af099137d8826e2cf9ea15cb18f2
Birger Skogeng Pedersen <birgersp@gmail.com>
2017-03-20 15:18:01 +0100

    Added features to lua script, implementing reconnections (WIP), #50

--- lua/mint.lua
+++ lua/mint.lua
@@ -1,11 +1,18 @@
 do
 
+  local ADDRESS = '127.0.0.1'
+  local PORT = 3001
+  local DATA_TIMEOUT_SEC = 5
+
   package.path = package.path..";.\\LuaSocket\\?.lua"
   package.cpath = package.cpath..";.\\LuaSocket\\?.dll"
 
   require = mint.require
   local socket = require("socket")
-  local client = socket.tcp()
+  require = nil
+
+  local tcp = socket.tcp()
+  local connected = false
 
   local function sendData()
     local msg = "{"
@@ -13,24 +20,24 @@ do
     local function addUnit(unit)
       msg = msg .. "[";
       msg = msg .. "\"" .. unit:getTypeName() .. "\""
-	  local unitPosition = unit:getPosition()
+      local unitPosition = unit:getPosition()
       local lat, lon, alt = coord.LOtoLL(unitPosition.p)
-	  local unitXYZNorthCorr = coord.LLtoLO(lat + 1, lon)
-	  local headingNorthCorr = math.atan2(unitXYZNorthCorr.z - unitPosition.p.z, unitXYZNorthCorr.x - unitPosition.p.x)
-	  local heading = math.atan2(unitPosition.x.z, unitPosition.x.x) + headingNorthCorr
-	  if heading < 0 then
-		heading = heading + 2 * math.pi
-	  end
-	  local headingDeg = math.floor(heading / math.pi * 180);
-	  
-	  local velocity = unit:getVelocity()
-	  local speed = math.sqrt(velocity.x^2 + velocity.z^2)
-	  
+      local unitXYZNorthCorr = coord.LLtoLO(lat + 1, lon)
+      local headingNorthCorr = math.atan2(unitXYZNorthCorr.z - unitPosition.p.z, unitXYZNorthCorr.x - unitPosition.p.x)
+      local heading = math.atan2(unitPosition.x.z, unitPosition.x.x) + headingNorthCorr
+      if heading < 0 then
+        heading = heading + 2 * math.pi
+      end
+      local headingDeg = math.floor(heading / math.pi * 180);
+
+      local velocity = unit:getVelocity()
+      local speed = math.sqrt(velocity.x^2 + velocity.z^2)
+
       msg = msg .. "," .. lat
       msg = msg .. "," .. lon
       msg = msg .. "," .. alt
-	  msg = msg .. "," .. headingDeg
-	  msg = msg .. "," .. speed
+      msg = msg .. "," .. headingDeg
+      msg = msg .. "," .. speed
       msg = msg .. "]";
     end
 
@@ -59,22 +66,23 @@ do
     local blueGroups = coalition.getGroups(coalition.side.BLUE)
     addGroups(blueGroups)
     msg = msg .. "]}\n"
-    client:send(msg)
+
+    return pcall(tcp.send, tcp, msg)
   end
 
   local function loop()
-    if not client then
-      return
+
+    if not connected then
+      connected = tcp:connect(ADDRESS, PORT)
     end
 
-    timer.scheduleFunction(loop, {}, timer.getTime() + 5)
-    sendData()
+    if connected then
+      connected = sendData()
+    end
+
+    timer.scheduleFunction(loop, {}, timer.getTime() + DATA_TIMEOUT_SEC)
   end
 
-  if client:connect('127.0.0.1', 3001) then
-    loop()
-  end
-
-  require = nil
+  loop()
 
 end
