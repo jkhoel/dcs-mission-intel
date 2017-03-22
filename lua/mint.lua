1303e5a85b88ffb23ab8be80c0d937dd9f6ac87e
Jens Kristian Hoel <github@hoelweb.com>
2017-03-22 17:43:58 +0100

    Change to transfer frequency from DCS

--- lua/mint.lua
+++ lua/mint.lua
@@ -1,7 +1,7 @@
 do
   --
   local PORT = 3001
-  local DATA_TIMEOUT_SEC = 1
+  local DATA_TIMEOUT_SEC = 3
 
   package.path = package.path..";.\\LuaSocket\\?.lua"
   package.cpath = package.cpath..";.\\LuaSocket\\?.dll"
